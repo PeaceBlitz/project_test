0dc96309146148de46853ad2e5100b90426d8e4e
wuyuegang <wuyuegang@baiwang.com>
2018-03-08 15:02:57 +0800

    update

---
+++ custom-dao/src/main/java/com/baiwang/boot/dao/DemoDao.java
@@ -0,0 +1,19 @@
+package com.baiwang.boot.dao;
+
+
+/**
+ * 模板测试接口，可以删除
+ * 
+ * @author wuyuegang
+ * 
+ */
+public interface DemoDao
+{
+    /**
+     * get product list from db
+     * 
+     * @return
+     */
+    public String getProducts();
+    
+}
